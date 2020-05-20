698717707e397b8a787e2557a86553c76f71fc6b
kevinMonteza <kmonteza95@gmail.com>
2020-05-19 23:32:07 -0500

    simple alert finished

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -6,12 +6,46 @@ class AlertPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('alert page'),),
+        appBar: AppBar(
+          title: Text('alert page'),
+        ),
         body: Center(
           child: RaisedButton(
             child: Text('Monstrar alerta'),
-            onPressed: null),),
+            textColor: Colors.white,
+            color: Colors.blue,
+            shape: StadiumBorder(),
+            onPressed: () => _showAlert(context),
+          ),
+        ));
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) => AlertDialog(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+        title: Text('Titulo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido'),
+            FlutterLogo(size: 100.0),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Cancelar'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('ok'),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        ],
+      ),
     );
   }
 }
